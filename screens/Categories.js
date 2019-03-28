e614114f339b8230cbb1023853f8aca14d7b6524
William Goodall <wgoodall01@gmail.com>
2019-03-28 18:08:34 -0400

    shadow() function, editor

--- screens/Categories.js
+++ screens/Categories.js
@@ -1,17 +1,44 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, Slider } from "react-native";
 import tabIcon from "../components/tabIcon";
 import SafeView from "../components/SafeView";
+import shadow from "../lib/shadow";
 
-const Categories = () => (
-  <SafeView>
-    <Text>Categories</Text>
-  </SafeView>
-);
+class Categories extends React.Component {
+  static navigationOptions = {
+    title: "Categories",
+    tabBarIcon: tabIcon("th")
+  };
 
-Categories.navigationOptions = {
-  title: "Categories",
-  tabBarIcon: tabIcon("th")
-};
+  state = {
+    amt: 0
+  };
+
+  render() {
+    const { amt } = this.state;
+    return (
+      <SafeView style={{ margin: 50 }}>
+        <Text>Shadow Sandbox</Text>
+        <View
+          style={{
+            ...shadow(amt),
+            width: 100,
+            height: 100,
+            borderRadius: 10,
+            backgroundColor: "white",
+            margin: 20
+          }}
+        />
+        <Text>{amt}</Text>
+        <Slider
+          min={0}
+          max={1}
+          value={amt}
+          onValueChange={e => this.setState({ amt: e })}
+        />
+      </SafeView>
+    );
+  }
+}
 
 export default Categories;
