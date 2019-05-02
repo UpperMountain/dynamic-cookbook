082767eeb1249ba46be969f3a2296eff32f8646b
William Goodall <wgoodall01@gmail.com>
2019-05-02 16:11:50 -0400

    update .eslintrc with typescript support

--- components/tabIcon.js
+++ components/tabIcon.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { FontAwesome } from "@expo/vector-icons";
 
-const tabIcon = name => ({ tintColor }) => (
-  <FontAwesome name={name} color={tintColor} size={24} />
-);
-
-export default tabIcon;
+export default function tabIcon(name) {
+  return function TabIcon({ tintColor }) {
+    return <FontAwesome name={name} color={tintColor} size={24} />;
+  };
+}
