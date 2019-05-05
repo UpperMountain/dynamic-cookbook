UpperMountain/dynamic-cookbook

commit f8ffde922094467e4cfac24827b679fecadf0cff

wip: recipe structure

.eslintrc.yml
@@ -56,6 +56,7 @@ rules:
   react/jsx-wrap-multilines: off
   react/no-multi-comp: off
   react/react-in-jsx-scope: off
+  "@typescript-eslint/no-parameter-properties": off
 
   # possibly useful, but annoying
   jsx-a11y/alt-text: off
@@ -65,8 +66,7 @@ rules:
   react/no-danger: off
   react/no-unescaped-entities: off
   "@typescript-eslint/explicit-member-accessibility": off
-  "@typescript-eslint/explicit-function-return-type":
-    [error, { allowExpressions: true }]
+  "@typescript-eslint/explicit-function-return-type": off
 
   # turn these on later, when we have time to clear the errors
   react/prop-types: off

data/dependencyTree.ts
@@ -7,30 +7,36 @@ export interface Timer {
   until: string;
 }
 
-export interface Ingredient {
-  kind: "ingredient";
-  name: string;
-}
-
-export interface Step {
-  kind: "step";
-
-  // display attributes for the ui
+export interface WithBody {
+  // for display in the UI
   name: string;
   body: string;
+}
+
+export interface Ingredient extends WithBody {
+  kind: "ingredient";
+  name: string;
+  amount: number; // TODO: physical quantities
+}
+
+export interface Step extends WithBody {
+  kind: "step";
 
   // timers
-  duration: Duration;
+  duration?: Duration; // active time during the step
   timer?: Timer; // passive time after the step
 
   // dependencies
-  requires: Node[];
+  requires: (Step | Ingredient)[];
 }
 
 // The final product---what we're cooking.
-export interface Recipe {
+export interface Recipe extends WithBody {
   kind: "recipe";
+
+  // dependencies
+  requires: Step[];
 }
 
 // union type discriminated on `kind`
-export type Node = Ingredient | Step;
+export type Node = Ingredient | Step | Recipe;

data/recipes/pasta.test.ts
@@ -1,0 +1,11 @@
+import { PastaRecipe } from "./pasta";
+
+it("should create a Pasta without error", () => {
+  new PastaRecipe(4);
+});
+
+it("should create Nodes from a Pasta without error", () => {
+  const a = new PastaRecipe(4);
+  const node = a.getNode();
+  console.log(node);
+});

data/recipes/pasta.ts
@@ -1,0 +1,205 @@
+import { Node, Step, Ingredient, Recipe } from "../dependencyTree";
+
+interface Procedure {
+  // Attempt to include another Procedure into this one.
+  // If successful, mutate self, and return true.
+  // If you can't do the merge, return false.
+  // merge() MUST be commutative, to run in O(n) on the graph.
+  // Think of it like a reduce().
+  merge(other: Procedure): Procedure | null;
+
+  // Must generate a list of children on construction
+  requires: Procedure[];
+
+  // generate the right Node for sequencing
+  getNode(): Node;
+}
+
+// typed 'this' param isn't actually a parameter.
+// This function should be attached to a class to work.
+function mergeByChildren(this: Procedure, other: Procedure) {
+  if (other instanceof this.constructor) {
+    // Merge the children
+    this.requires.concat(other.requires);
+    return this;
+  }
+
+  return null;
+}
+
+// TODO: test this before using it.
+// function mergeByReduce<T extends Procedure>(
+//   this: T,
+//   reducer: (other: Procedure) => T | undefined
+// ) {
+//   return function(this: Procedure, other: Procedure) {
+//     if (other instanceof this.constructor) {
+//       return reducer(other) || this;
+//     }
+//     return null;
+//   };
+// }
+
+// ///////////////////////////////////////////////////////////////////////////////////
+
+export class Onion implements Procedure {
+  constructor(public count: number) {}
+  getNode(): Ingredient {
+    return {
+      kind: "ingredient",
+      name: "Onion",
+      body: "something something shrek quote",
+      amount: this.count
+    };
+  }
+
+  merge(other: Procedure) {
+    if (other instanceof Onion) {
+      this.count += other.count;
+      return this;
+    }
+
+    return null;
+  }
+
+  requires = [];
+}
+
+export class Garlic implements Procedure {
+  constructor(public cloves: number) {}
+  getNode(): Ingredient {
+    return {
+      kind: "ingredient",
+      name: "Garlic",
+      body: "is it just called... a garlic? a garlic... bulb? a head?",
+      amount: Math.ceil(this.cloves / 4)
+    };
+  }
+
+  merge(other: Procedure) {
+    if (other instanceof Garlic) {
+      this.cloves += other.cloves;
+      return this;
+    }
+
+    return null;
+  }
+
+  requires = [];
+}
+
+export class PastaSauce implements Procedure {
+  constructor(public serves: number) {}
+  getNode(): Step {
+    return {
+      kind: "step",
+      name: "Start the sauce",
+      body: `
+        Throw the garlic in a pan with some oil. Over medium heat, cook until the
+        garlic starts to brown.
+
+        Add onion, oregano, and some salt. Cook until the onion is soft.
+
+        Add the tomatoes (with the juicy stuff) and cook for about 20 minutes, or
+        until the sauce is reduced.
+      `,
+      timer: { duration: 20, until: "reduced slightly" },
+      requires: this.requires.map(e => e.getNode())
+    };
+  }
+
+  // this is technically a whole clove of garlic, but it's also a proof-of-concept example
+  requires = [new Garlic(2 * this.serves), new Onion(0.5 * this.serves)];
+
+  merge = mergeByChildren;
+}
+
+export class Spaghetti implements Procedure {
+  constructor(public serves: number) {}
+  getNode(): Ingredient {
+    return {
+      kind: "ingredient",
+      name: "Spaghetti",
+      body: "It's just spaghetti.",
+      amount: this.serves * 12
+    };
+  }
+
+  merge(other: Procedure) {
+    if (other instanceof Spaghetti) {
+      this.serves += other.serves;
+      return this;
+    }
+
+    return null;
+  }
+
+  requires = [];
+}
+
+export class Pasta implements Procedure {
+  constructor(public serves: number) {}
+  getNode(): Step {
+    return {
+      kind: "step",
+      name: "Start the pasta",
+      body: `
+      Once the water comes to a boil, throw in the pasta.
+
+      Boil it for the time written on the box (generally 9-10 minutes) or until
+      [it's al dente][al-dente].
+    `,
+      timer: { duration: 9, until: "al dente" },
+      requires: this.requires.map(e => e.getNode())
+    };
+  }
+  merge = mergeByChildren;
+  requires = [new Spaghetti(this.serves)];
+}
+
+export class PastaCombine implements Procedure {
+  constructor(public serves: number) {}
+  getNode(): Step {
+    return {
+      kind: "step",
+      name: "Combine the sauce and the pasta",
+      body: `
+        When the pasta is done, strain the water out of the pot. Throw in some butter.
+
+        When the sauce is done, take it off the heat.
+
+        Pour the sauce over the pasta, and shake the pot around until covered.
+      `,
+      requires: this.requires.map(e => e.getNode())
+    };
+  }
+  merge = mergeByChildren;
+  requires = [new Pasta(this.serves), new PastaSauce(this.serves)];
+}
+
+export class PastaRecipe implements Procedure {
+  constructor(public serves: number) {}
+  getNode(): Recipe {
+    return {
+      kind: "recipe",
+      name: `Pasta recipe, serves ${this.serves}`,
+      body: `
+        This is a _test_, with *rich* formatting from Markdown.
+
+        Do paragraphs work? Cool!
+      `,
+      requires: this.requires.map(e => e.getNode())
+    };
+  }
+
+  merge(other: Procedure) {
+    if (other instanceof PastaRecipe) {
+      this.serves += other.serves;
+      return this;
+    }
+
+    return null;
+  }
+
+  requires = [new PastaCombine(this.serves)];
+}

data/schema.test.js
@@ -1,64 +1,0 @@
-import { matchers as schemaMatchers } from "jest-json-schema";
-import yaml from "js-yaml";
-import fs from "fs";
-import { resolve } from "path";
-import { promisify } from "util";
-import schema from "./schema.json";
-import { yamlSchema } from "./yamlTypes";
-
-const readFile = promisify(fs.readFile);
-
-// install json-schema Jest matchers
-expect.extend(schemaMatchers);
-
-let pasta;
-beforeAll(async () => {
-  const ymlFile = await readFile(
-    resolve(__dirname, "./recipes/pasta.yml"),
-    "utf-8"
-  );
-  const parsed = yaml.loadAll(ymlFile, null, { schema: yamlSchema });
-  pasta = parsed;
-});
-
-describe("JSON schema", () => {
-  it("should be valid", () => {
-    expect(schema).toBeValidSchema();
-  });
-});
-
-const testAll = fn => {
-  for (const item of pasta) {
-    try {
-      fn(item);
-    } catch (err) {
-      console.warn(item);
-      throw err;
-    }
-  }
-};
-
-describe("all data", () => {
-  it("should pass schema validation", () => {
-    testAll(item => {
-      expect(item).toMatchSchema(schema);
-    });
-  });
-
-  it("should have an id for each item", () => {
-    testAll(item => {
-      expect(item.id).toBeDefined();
-      expect(item.id._id).toBeDefined();
-    });
-  });
-
-  it("should have no duplicate ids", () => {
-    const ids = new Set();
-
-    testAll(item => {
-      const id = item.id._id;
-      expect(ids.has(id)).toBe(false);
-      ids.add(item.id._id);
-    });
-  });
-});

data/yamlTypes.js
@@ -1,19 +1,0 @@
-import yaml from "js-yaml";
-
-export const yamlId = new yaml.Type("!id", {
-  kind: "scalar",
-
-  construct(data) {
-    return { _id: data };
-  }
-});
-
-export const yamlRef = new yaml.Type("!ref", {
-  kind: "scalar",
-
-  construct(data) {
-    return { _ref: data };
-  }
-});
-
-export const yamlSchema = yaml.Schema.create([yamlId, yamlRef]);

commit 9870c0c500b772f1375b334b94117a7bea2daeff

testing: fix coverage regression in f8ffde92, improve typescript support

.eslintrc.yml
@@ -1,7 +1,7 @@
 parser: "@typescript-eslint/parser"
 parserOptions:
   sourceType: module
-  project: ./tsconfig.json
+  project: tsconfig.json
   tsconfigRootDir: ./
   ecmaVersion: 2019
   ecmaFeatures:
@@ -34,13 +34,20 @@ env:
 
 overrides:
   # Allow console in tests
-  - files: "**/*.test.js"
+  - files: "**/*.test.{ts,js,tsx}"
     rules:
       no-console: off
+      "@typescript-eslint/no-non-null-assertion": off
+  - files: "**/*.config.js"
+    rules:
+      "@typescript-eslint/no-var-requires": off
 
 rules:
   jest/no-disabled-tests: error
   prettier/prettier: error
+
+  # configuration
+  "@typescript-eslint/no-unused-vars": ["error", { varsIgnorePattern: "^_" }]
 
   # useless
   class-methods-use-this: off
@@ -49,7 +56,6 @@ rules:
   no-restricted-syntax: [off, "BinaryExpression[operator='in']"]
   no-return-assign: [error, except-parens]
   no-underscore-dangle: off
-  no-unused-vars: [error, { argsIgnorePattern: "^_" }]
   react/jsx-closing-tag-location: off
   react/jsx-filename-extension: off
   react/jsx-one-expression-per-line: off

jest.config.js
@@ -1,0 +1,29 @@
+const expoPreset = require("jest-expo/jest-preset");
+
+expoPreset.transform = {
+  ...expoPreset.transform,
+  "^.+\\.{ts,tsx}$": "ts-jest"
+};
+
+expoPreset.testMatch = [
+  "**/__tests__/**/*.{js,ts,tsx}",
+  "**/?(*.)+(spec|test).{js,ts,tsx}"
+];
+
+expoPreset.moduleFileExtensions = ["js", "ts", "tsx"];
+
+expoPreset.globals = {
+  "ts-jest": {
+    tsConfig: {
+      jsx: "react"
+    }
+  }
+};
+
+expoPreset.collectCoverageFrom = [
+  "./{**/,}*.{js,ts,tsx}",
+  "!./{**/,}*.config.js",
+  "!./coverage/**"
+];
+
+module.exports = expoPreset;

screens/__snapshots__/Categories.test.js.snap
@@ -1,0 +1,58 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`passes snapshot tests 1`] = `
+<RCTSafeAreaView
+  style={
+    Object {
+      "flex": 1,
+      "margin": 50,
+      "paddingTop": 0,
+    }
+  }
+>
+  <Text>
+    Shadow Sandbox
+  </Text>
+  <View
+    style={
+      Object {
+        "backgroundColor": "white",
+        "borderRadius": 10,
+        "elevation": 0,
+        "height": 100,
+        "margin": 20,
+        "shadowColor": "#000",
+        "shadowOffset": Object {
+          "height": 1,
+          "width": 0,
+        },
+        "shadowOpacity": 0,
+        "shadowRadius": 0,
+        "width": 100,
+      }
+    }
+  />
+  <Text>
+    0
+  </Text>
+  <RCTSlider
+    disabled={false}
+    enabled={true}
+    max={1}
+    maximumValue={1}
+    min={0}
+    minimumValue={0}
+    onChange={[Function]}
+    onResponderTerminationRequest={[Function]}
+    onStartShouldSetResponder={[Function]}
+    onValueChange={[Function]}
+    step={0}
+    style={
+      Object {
+        "height": 40,
+      }
+    }
+    value={0}
+  />
+</RCTSafeAreaView>
+`;

scripts/ci.sh
@@ -11,6 +11,9 @@ echo "----- Code formatting"
 yarn lint
 echo
 
+# Type check
+echo "----- Type checking"
+yarn types
 
 # Run all tests
 echo "----- Test suite"

screens/Categories.test.js
@@ -1,0 +1,12 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Categories from "./Categories";
+
+it("renders without exploding", () => {
+  renderer.create(<Categories />);
+});
+
+it("passes snapshot tests", () => {
+  const cmp = renderer.create(<Categories />);
+  expect(cmp.toJSON()).toMatchSnapshot();
+});