d44721b3113d960e9e311eb11d66d23e5528a688
William Goodall <wgoodall01@gmail.com>
2019-05-21 23:06:53 -0400

    nav persistence, alert on crash, more detailed debug information

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { StatusBar } from "react-native";
-import { Font, AppLoading } from "expo";
+import { Alert, StatusBar, AsyncStorage } from "react-native";
+import { Font, AppLoading, ErrorRecovery } from "expo";
 import { MaterialIcons } from "@expo/vector-icons";
 import { createAppContainer } from "react-navigation";
 import { RootNavigator } from "./screens";
 import Sentry from "sentry-expo";
+
+export const navStatePersistenceKey = "navStatePersistence";
 
 // Create root app navigator
 const AppContainer = createAppContainer(RootNavigator);
@@ -17,13 +19,38 @@ Sentry.config(
 export default class App extends React.Component {
   state = { ready: false };
 
-  _loadResources = async () => {
-    // Load all fonts and assets
+  crashed: boolean = false;
+  constructor(props: any) {
+    super(props);
 
+    // @ts-ignore
+    const { exp } = this.props;
+    if (typeof exp.errorRecovery !== "undefined") {
+      console.log("found crash");
+      this.crashed = true;
+    }
+
+    console.log("set recovery props");
+    ErrorRecovery.setRecoveryProps({ didCrash: true });
+  }
+
+  private async loadResources(this: any) {
+    // Reset storage on crash
+    console.log("loadResources()");
+    if (this.crashed) {
+      console.log("crash alert");
+      Alert.alert(
+        "Issue reported",
+        "Cookie just closed unexpectedly.\n\nThis issue has been reported to the developers, and the app has been reset to (hopefully) working condition."
+      );
+      await AsyncStorage.clear();
+    }
+
+    // Load all fonts and assets
     await Font.loadAsync({
       ...MaterialIcons.font
     });
-  };
+  }
 
   render() {
     const { ready } = this.state;
@@ -32,13 +59,13 @@ export default class App extends React.Component {
       return (
         <>
           <StatusBar barStyle="default" />
-          <AppContainer />
+          <AppContainer persistenceKey={navStatePersistenceKey} />
         </>
       );
     }
     return (
       <AppLoading
-        startAsync={this._loadResources}
+        startAsync={this.loadResources.bind(this)}
         onFinish={() => this.setState({ ready: true })}
       />
     );
--- screens/Debug.tsx
+++ screens/Debug.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import {
+  AsyncStorage,
+  ScrollView,
+  View,
+  TouchableOpacity,
+  Text,
+  Platform
+} from "react-native";
+import { Updates, Constants } from "expo";
 
 function crash() {
   throw new Error("Crash test for Sentry");
@@ -21,7 +29,8 @@ function Info({ name, children }: { name: string; children: React.ReactNode }) {
   return (
     <View
       style={{
-        padding: 10,
+        margin: 10,
+        marginTop: 0,
         display: "flex",
         flexDirection: "row",
         alignItems: "center"
@@ -32,27 +41,88 @@ function Info({ name, children }: { name: string; children: React.ReactNode }) {
           width: 100,
           color: "gray",
           textAlign: "right",
-          marginRight: 10
+          marginRight: 10,
+          fontSize: 11
         }}
       >
         {name}
       </Text>
-      <Text style={{ flex: 1 }}>{children}</Text>
+      <Text style={{ flex: 1, fontSize: 11 }}>{children}</Text>
     </View>
+  );
+}
+
+function Button({
+  children,
+  onPress
+}: {
+  children: React.ReactNode;
+  onPress: () => void;
+}) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{ margin: 10, padding: 10, backgroundColor: "rgba(0,0,0,0.05)" }}
+    >
+      <Text>{children}</Text>
+    </TouchableOpacity>
+  );
+}
+
+function Header({ children }: { children: string }) {
+  return (
+    <Text style={{ margin: 20, marginTop: 30, fontSize: 20 }}>{children}</Text>
   );
 }
 
 export default function Debug() {
   return (
-    <View style={{ padding: 30 }}>
+    <ScrollView
+      style={{ padding: 10 }}
+      contentContainerStyle={{ paddingBottom: 50 }}
+    >
+      <Header>Diagnostics</Header>
+      <Button onPress={() => crash()}>
+        Crash the app (sentry reporting test)
+      </Button>
+      <Button onPress={() => Updates.reload()}>
+        Force OTA update to latest
+      </Button>
+      <Button onPress={() => AsyncStorage.clear()}>Clear AsyncStorage</Button>
+      <Header>Build information</Header>
       <Info name="NODE_ENV">{env.NODE_ENV}</Info>
       <Info name="Node Version">{env.nodeVersion}</Info>
       <Info name="Commit">{env.commit}</Info>
       <Info name="Commit Message">{env.commitMessage}</Info>
       <Info name="Job Number">{env.buildJobNumber}</Info>
-      <TouchableOpacity onPress={() => crash()}>
-        <Text>Crash the app (sentry test)</Text>
-      </TouchableOpacity>
-    </View>
+      <Header>Constants</Header>
+      <Info name="Expo Version">{Constants.expoVersion}</Info>
+      <Info name="App Ownership">{Constants.appOwnership}</Info>
+      <Info name="Installation ID">{Constants.installationId}</Info>
+      <Info name="Session ID">{Constants.sessionId}</Info>
+      <Info name="Year Class">{Constants.deviceYearClass}</Info>
+      <Header>Platform</Header>
+      {Platform.select({
+        /* eslint-disable react/display-name */
+        ios: () => (
+          <>
+            <Info name="Platform">iOS</Info>
+            <Info name="Version">{Platform.Version}</Info>
+            <Info name="Model">{Constants.platform.ios!.model}</Info>
+            <Info name="Internal ID">{Constants.platform.ios!.platform}</Info>
+          </>
+        ),
+        android: () => (
+          <>
+            <Info name="Platform">Android</Info>
+            <Info name="Version">{Platform.Version}</Info>
+            <Info name="Version Code">
+              {Constants.platform.android!.versionCode}
+            </Info>
+          </>
+        )
+        /* eslint-enable */
+      })()}
+    </ScrollView>
   );
 }
