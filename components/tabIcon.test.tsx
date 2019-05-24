12f898d4a39b45a5a9d837a83ed7dbe228e1a8ac
William Goodall <wgoodall01@gmail.com>
2019-05-24 18:35:01 -0400

    add communityTabIcon for MaterialCommunityIcons

--- components/tabIcon.test.tsx
+++ components/tabIcon.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import tabIcon from "./tabIcon";
+import tabIcon, { communityTabIcon } from "./tabIcon";
 import renderer from "react-test-renderer";
 
 describe('tabIcon("home")', () => {
@@ -13,6 +13,24 @@ describe('tabIcon("home")', () => {
       <>
         <HomeIcon />
         <HomeIcon tintColor="red" />
+      </>
+    );
+    expect(cmp.toJSON()).toMatchSnapshot();
+    cmp.unmount();
+  });
+});
+
+describe('communityTabIcon("account")', () => {
+  const AccountIcon = communityTabIcon("account");
+  it("should render without exploding", () => {
+    renderer.create(<AccountIcon />).unmount();
+  });
+
+  it("should pass snapshot tests", () => {
+    const cmp = renderer.create(
+      <>
+        <AccountIcon />
+        <AccountIcon tintColor="red" />
       </>
     );
     expect(cmp.toJSON()).toMatchSnapshot();
--- components/tabIcon.tsx
+++ components/tabIcon.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 interface Props {
   tintColor?: string;
@@ -8,5 +8,11 @@ interface Props {
 export default function tabIcon(name: string) {
   return function TabIcon({ tintColor }: Props) {
     return <MaterialIcons name={name} color={tintColor} size={24} />;
+  };
+}
+
+export function communityTabIcon(name: string) {
+  return function TabIcon({ tintColor }: Props) {
+    return <MaterialCommunityIcons name={name} color={tintColor} size={24} />;
   };
 }
